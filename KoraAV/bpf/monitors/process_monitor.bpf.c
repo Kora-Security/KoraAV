5a76af9a6b86084e5d224d6a8a87639ea705e5e9
Ori <45724082+therealOri@users.noreply.github.com>
2026-02-18 19:13:36 -0700

    Update process_monitor.bpf.c

--- KoraAV/bpf/monitors/process_monitor.bpf.c
+++ KoraAV/bpf/monitors/process_monitor.bpf.c
@@ -128,7 +128,7 @@ static __always_inline bool check_rate_limit(__u32 pid) {
 SEC("tracepoint/syscalls/sys_enter_execve")
 int trace_execve(struct trace_event_raw_sys_enter *ctx) {
     __u64 pid_tgid = bpf_get_current_pid_tgid();
-    __u32 pid = pid_tgid >> 32;
+    __u32 tgid = pid_tgid >> 32;
     __u32 uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
     
     // FILTER #1: Skip kernel threads
