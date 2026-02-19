f54b94db9eaa5dfba5cb4a58f3c964048130ee83
Ori <45724082+therealOri@users.noreply.github.com>
2026-02-18 19:21:53 -0700

    Update file_monitor.bpf.c

--- KoraAV/bpf/monitors/file_monitor.bpf.c
+++ KoraAV/bpf/monitors/file_monitor.bpf.c
@@ -127,8 +127,8 @@ static __always_inline bool check_rate_limit(__u32 pid) {
 
 SEC("tracepoint/syscalls/sys_enter_openat")
 int trace_openat(struct trace_event_raw_sys_enter *ctx) {
-    __u64 pid_tgid = bpf_get_current_pid_tgid();
-    __u32 pid = pid_tgid >> 32;
+    u64 pid_tgid = bpf_get_current_pid_tgid();
+    u32 pid = pid_tgid >> 32;
     __u32 uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
     
     // FILTER #1: Skip kernel threads (PID 0)
